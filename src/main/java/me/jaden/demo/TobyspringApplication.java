3d2722066088d5264e5ae422a63d2c831ec0b31f
jaden <jonghaha7@gmail.com>
2023-06-10 14:38:03 +0900

    standAlone 프로그램을 만들기 위해 embedded tomcat servlet container 생성 후 servlet 등록

--- src/main/java/me/jaden/demo/TobyspringApplication.java
+++ src/main/java/me/jaden/demo/TobyspringApplication.java
@@ -1,13 +1,34 @@
 package me.jaden.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class TobyspringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TobyspringApplication.class, args);
-	}
+    public static void main(String[] args) {
+		// tomcat servletContainer 생성
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		// servlet 등록
+        serverFactory.getWebServer(servletContext -> servletContext.addServlet("helloServlet", new HttpServlet() {
+            @Override
+            protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+                    IOException {
+                String name = req.getParameter("name");
+
+                resp.setStatus(HttpStatus.OK.value());
+                resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                resp.getWriter().print("hello " + name);
+            }
+        }));
+    }
 
 }

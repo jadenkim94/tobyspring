43c83800e66954b85e9c622e535067faa54a44f2
jaden <jonghaha7@gmail.com>
2023-06-11 18:36:14 +0900

    factory 메소드를 통해 bean 등록하기

--- src/main/java/me/jaden/demo/HelloController.java
+++ src/main/java/me/jaden/demo/HelloController.java
@@ -1,7 +1,13 @@
 package me.jaden.demo;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping
 public class HelloController {
 
     private final HelloSerivce helloSerivce;
@@ -10,6 +16,8 @@ public class HelloController {
         this.helloSerivce = helloSerivce;
     }
 
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name) {
         return helloSerivce.sayHello(Objects.requireNonNull(name));
     }
--- src/main/java/me/jaden/demo/TobyspringApplication.java
+++ src/main/java/me/jaden/demo/TobyspringApplication.java
@@ -3,52 +3,44 @@ package me.jaden.demo;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
+@Configuration
 public class TobyspringApplication {
+
+    @Bean
+    public HelloController helloController(HelloSerivce helloSerivce) {
+        return new HelloController(helloSerivce);
+    }
+
+    @Bean
+    public HelloSerivce helloSerivce() {
+        return new SimpleHelloService();
+    }
+
+
     public static void main(String[] args) {
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this)
+                    ).addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
+        applicationContext.register(TobyspringApplication.class);
         applicationContext.refresh();
-        // tomcat servletContainer 생성
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		// frontController 등록
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("frontcontroller"
-                    , new HttpServlet() {
-                        @Override
-                        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
-                                IOException {
-                            // 공통적으로 해야할 인증, 보안, 다국어처리를 하는 코드 삽입
 
-                            HelloController helloController = applicationContext.getBean(HelloController.class);
 
-                            // hello 요청 매핑
-                            if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                                String name = req.getParameter("name");
-
-                                String ret = helloController.hello(name);
-                                resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                                resp.getWriter().print(ret);
-                            } else {
-                                resp.setStatus(HttpStatus.NOT_FOUND.value());
-                            }
-                        }
-                        // 모든 요청을 mapping
-                    }).addMapping("/*");
-        });
-        webServer.start();
     }
 
 }

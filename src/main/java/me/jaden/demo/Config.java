321c7bf906b7d2da957673ac21225a74ef647135
jaden <jonghaha7@gmail.com>
2023-06-20 15:27:30 +0900

    @SpringBootApplication 대체 1

--- src/main/java/me/jaden/demo/Config.java
+++ src/main/java/me/jaden/demo/Config.java
@@ -1,19 +1,13 @@
 package me.jaden.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@MySpringBootAnnotation
-public class TobyspringApplication {
+@Configuration
+public class Config {
     @Bean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
@@ -22,8 +16,5 @@ public class TobyspringApplication {
     @Bean
     public DispatcherServlet dispatcherServlet() {
         return new DispatcherServlet();
-    }
-    public static void main(String[] args) {
-        SpringApplication.run(TobyspringApplication.class, args);
     }
 }
--- src/test/java/me/jaden/demo/HelloServiceTest.java
+++ src/test/java/me/jaden/demo/HelloServiceTest.java
@@ -2,23 +2,10 @@ package me.jaden.demo;
 
 import org.junit.jupiter.api.Test;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import static org.assertj.core.api.Assertions.assertThat;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-@Test
-@interface UnitTest {
-
-}
 
 class HelloServiceTest {
 
-    @UnitTest
     void simpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
         String ret = helloService.sayHello("Test");

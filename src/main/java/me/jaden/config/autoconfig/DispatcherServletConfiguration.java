bda1e088df15f03c2a607c94d89b8f0c5eba0f62
jaden <jonghaha7@gmail.com>
2023-06-21 13:51:24 +0900

    자동 구성 애노테이션 적용

--- src/main/java/me/jaden/config/autoconfig/DispatcherServletConfiguration.java
+++ src/main/java/me/jaden/config/autoconfig/DispatcherServletConfiguration.java
@@ -1,8 +1,10 @@
 package me.jaden.config.autoconfig;
 
+import me.jaden.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@MyAutoConfiguration
 public class DispatcherServletConfiguration {
     @Bean
     public DispatcherServlet dispatcherServlet() {
--- src/main/java/me/jaden/config/autoconfig/TomcatWebServerConfiguration.java
+++ src/main/java/me/jaden/config/autoconfig/TomcatWebServerConfiguration.java
@@ -1,9 +1,11 @@
 package me.jaden.config.autoconfig;
 
+import me.jaden.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
+@MyAutoConfiguration
 public class TomcatWebServerConfiguration {
 
     @Bean

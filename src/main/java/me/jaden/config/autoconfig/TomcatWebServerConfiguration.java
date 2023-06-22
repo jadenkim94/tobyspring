76d8a7b9be5f205e637b5d88ee09765e470e144b
jaden <jonghaha7@gmail.com>
2023-06-22 12:17:13 +0900

    자동구성에 Environment 프로퍼티 적용

--- src/main/java/me/jaden/config/autoconfig/TomcatWebServerConfiguration.java
+++ src/main/java/me/jaden/config/autoconfig/TomcatWebServerConfiguration.java
@@ -5,13 +5,16 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfiguration {
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory servletWebServerFactory(Environment environment) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setContextPath(environment.getProperty("contextPath"));
+        return factory;
     }
 }

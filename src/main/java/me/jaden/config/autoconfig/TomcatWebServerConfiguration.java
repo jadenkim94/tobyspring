17c9daec69ff5bc26e4ff01d90358a74943fdd3d
jaden <jonghaha7@gmail.com>
2023-06-22 12:24:13 +0900

    @Value 와 PropertyPlaceHolderConfigurer

--- src/main/java/me/jaden/config/autoconfig/TomcatWebServerConfiguration.java
+++ src/main/java/me/jaden/config/autoconfig/TomcatWebServerConfiguration.java
@@ -1,20 +1,23 @@
 package me.jaden.config.autoconfig;
 
 import me.jaden.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfiguration {
+    @Value("${contextPath}")
+    private String contextPath;
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory(Environment environment) {
+    public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(environment.getProperty("contextPath"));
+        factory.setContextPath(contextPath);
         return factory;
     }
 }
